473bc30e3738815bde28692b6a61c170c0589933
Anna Belanova <anna.n.belanova@gmail.com>
2016-01-17 16:55:56 +0100

    Added GUI with test 3 entries

---
+++ WindowsFileManager/FileDirModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WindowsFileManager
+{
+	public class FileDirModel
+	{
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public int Size { get; set; }
+		public DateTime LastModificationDate { get; set; }
+		
+	}
+}
---
+++ WindowsFileManager/FileDirViewMOdel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFileManager
+{
+	public class FileDirViewModel
+	{
+		public List<FileDirModel> ViewData { get; set; }
+		
+		public FileDirViewModel()
+		{
+			ViewData = new List<FileDirModel>();
+
+			ViewData.Add(new FileDirModel { Name = "Folder1", Type = "Dir", Size = 0, LastModificationDate = DateTime.Now });
+			ViewData.Add(new FileDirModel { Name = "Folder2", Type = "Dir", Size = 21230, LastModificationDate = DateTime.Now });
+			ViewData.Add(new FileDirModel { Name = "File1", Type = "txt", Size = 3000, LastModificationDate = DateTime.Now });
+		}
+	}
+}
